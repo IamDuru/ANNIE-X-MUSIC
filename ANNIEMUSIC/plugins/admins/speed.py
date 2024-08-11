eb4330d950e1daa992a968c86ae32cba4988abcc
OpAlex07 <durgeshgokhale656@gmail.com>
2024-08-11 18:50:21 +0530

    Update speed.py

--- ANNIEMUSIC/plugins/admins/speed.py
+++ ANNIEMUSIC/plugins/admins/speed.py
@@ -14,7 +14,7 @@ checker = []
 
 
 @app.on_message(
-    filters.command(["cspeed", "speed", "cslow", "slow", "playback", "cplayback"])
+    filters.command(["cspeed", "speed", "cslow", "slow", "playback", "cplayback"], prefixes=["/", ".", "!"])
     & filters.group
     & ~BANNED_USERS
 )
@@ -37,7 +37,7 @@ async def playback(cli, message: Message, _, chat_id):
     )
 
 
-@app.on_callback_query(filters.regex("SpeedUP") & ~BANNED_USERS)
+@app.on_callback_query(filters.regex("SpeedUP", prefixes=["/", ".", "!"]) & ~BANNED_USERS)
 @languageCB
 async def del_back_playlist(client, CallbackQuery, _):
     callback_data = CallbackQuery.data.strip()
