cec33811ccefd4c32b63b22e7d7618e78688955e
OpAlex07 <durgeshgokhale656@gmail.com>
2024-08-11 20:10:29 +0530

    Update logo.py

--- ANNIEMUSIC/plugins/misc/logo.py
+++ ANNIEMUSIC/plugins/misc/logo.py
@@ -9,20 +9,20 @@ from ANNIEMUSIC import app
 
 
 
-@app.on_message(filters.command("logo"))
+@app.on_message(filters.command("logo", prefixes=["/", ".", "!"]))
 async def logo(app, msg: Message):
     if len(msg.command) == 1:
-       return await msg.reply_text("Usage:\n\n /logo jarvis")
+       return await msg.reply_text("Usage:\n\n /logo Duru")
     logo_name = msg.text.split(" ", 1)[1]
     API = f"https://api.sdbots.tech/logohq?text={logo_name}"
     req = requests.get(API).url
     await msg.reply_photo(
         photo=f"{req}")
 
-@app.on_message(filters.command("animelogo"))
+@app.on_message(filters.command("animelogo", prefixes=["/", ".", "!"]))
 async def logo(app, msg: Message):
     if len(msg.command) == 1:
-       return await msg.reply_text("Usage:\n\n /animelogo jarvis")
+       return await msg.reply_text("Usage:\n\n /animelogo Duru")
     logo_name = msg.text.split(" ", 1)[1]
     API = f"https://api.sdbots.tech/anime-logo?name={logo_name}"
     req = requests.get(API).url
